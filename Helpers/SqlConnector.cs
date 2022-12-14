60a836719bf7fe1088fc07bb72a7724814c9779d
Chengxiao Gong <gcx@mail.nwpu.edu.cn>
2022-12-15 01:17:56 +0800

    数据库连接helper

---
+++ Helpers/SqlConnector.cs
@@ -0,0 +1,7 @@
+﻿
+namespace AssetManager.Helpers;
+public class SqlConnector
+{
+    private string _host = "rm-2vclo609409s1ky214o.mysql.cn-chengdu.rds.aliyuncs.com";
+    private string _port = "3306";
+}
