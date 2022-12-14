95751c41309edda30e0f061bb3d9a683b249c453
Chengxiao Gong <gcx@mail.nwpu.edu.cn>
2022-12-15 00:44:37 +0800

    greetings text

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -1,10 +1,28 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using AssetManager.Helpers;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace AssetManager.ViewModels;
 
 public class HomeViewModel : ObservableRecipient
 {
+    private string _greetingMessage;
+
+    public string GreetingMessage
+    {
+        get => _greetingMessage;
+    }
+    
     public HomeViewModel()
     {
+        _greetingMessage = GetGreetingMessage();
+    }
+
+    private string GetGreetingMessage()
+    {
+        var hour = DateTime.Now.Hour;
+        var greeting =    hour < 12 ? "Home_MorningGreeting".GetLocalized() 
+                        : hour < 18 ?  "Home_AfternoonGreeting".GetLocalized()
+                        : "Home_EveningGreeting".GetLocalized();
+        return $"{greeting}, {Environment.UserName}!";
     }
 }
