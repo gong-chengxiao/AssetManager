6c6fcde14a152341386350e124371d2d08fb460a
Chengxiao Gong <gcx@mail.nwpu.edu.cn>
2022-12-18 19:26:42 +0800

    实现Filter Search Refresh功能

--- ViewModels/AssetTableViewModel.cs
+++ ViewModels/AssetTableViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Data;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
@@ -37,12 +38,32 @@ public class AssetTableViewModel : ObservableRecipient, INavigationAware, INotif
     {
         get;
     }
-
+    public ICommand TimeFilterCommand
+    {
+        get;
+    }
+    public ICommand SubmittCommand
+    {
+        get;
+    }
+    public ICommand DeleteRowCommand
+    {
+        get;
+    }
+    public ICommand AddCommand
+    {
+        get;
+    }
     public ObservableCollection<SchoolAsset> Source { get; } = new ObservableCollection<SchoolAsset>();
-
+    public SchoolAsset SelectedRow
+    {
+        get; set;
+    }
     public AssetTableViewModel(IAssetDataService assetDataService)
     {
+        ProgressBarVisibility = Visibility.Collapsed;
         _assetDataService = assetDataService;
+
         FileterCommand = new RelayCommand<KeyValueStringPair>(
             async (param) =>
             {
@@ -71,7 +92,7 @@ public class AssetTableViewModel : ObservableRecipient, INavigationAware, INotif
                 ProgressBarVisibility = Visibility.Visible;
                 try
                 {
-                    var data = await _assetDataService.RefreshGridDataAsync();
+                    var data = await _assetDataService.GetRefreshGridDataAsync();
                     Source.Clear();
                     foreach (var asset in data)
                     {
@@ -87,13 +108,109 @@ public class AssetTableViewModel : ObservableRecipient, INavigationAware, INotif
                     ProgressBarVisibility = Visibility.Collapsed;
                 }
             });
+        TimeFilterCommand = new RelayCommand<string>(
+           async (param) =>
+           {
+               ProgressBarVisibility = Visibility.Visible;
+               try
+               {
+                   var data = await _assetDataService.GetLastMonthGridDataAsync(param);
+                   Source.Clear();
+                   foreach (var asset in data)
+                   {
+                       Source.Add(asset);
+                   }
+               }
+               catch (Exception e)
+               {
+                   await NotifyHelper.ShowNotifyDialog(NotifyHelper.ErrorTitle, e.Message);
+               }
+               finally
+               {
+                   ProgressBarVisibility = Visibility.Collapsed;
+               }
+
+           });
+        SubmittCommand = new RelayCommand(
+            async () =>
+            {
+                ProgressBarVisibility = Visibility.Visible;
+                try
+                {
+                    await _assetDataService.ActivateUpdateList();
+                }
+                catch (Exception e)
+                {
+                    await NotifyHelper.ShowNotifyDialog(NotifyHelper.ErrorTitle, e.Message);
+                }
+                finally
+                {
+                    ProgressBarVisibility = Visibility.Collapsed;
+                }
+            });
+        DeleteRowCommand = new RelayCommand(
+            async () =>
+            {
+                ProgressBarVisibility = Visibility.Visible;
+                try
+                {
+                    await _assetDataService.DeleteRowAsync(SelectedRow.AssetID);
+                    var data = await _assetDataService.GetRefreshGridDataAsync();
+                    Source.Clear();
+                    foreach (var asset in data)
+                    {
+                        Source.Add(asset);
+                    }
+                }
+                catch (Exception e)
+                {
+                    await NotifyHelper.ShowNotifyDialog(NotifyHelper.ErrorTitle, e.Message);
+                }
+                finally
+                {
+                    ProgressBarVisibility = Visibility.Collapsed;
+                }
+            });
+        AddCommand = new RelayCommand(
+            () =>
+            {
+                Source.Insert(0, new());
+            });
+    }
+    public async Task SearchBoxQuery(string param)
+    {
+
+        ProgressBarVisibility = Visibility.Visible;
+        try
+        {
+            await Task.Delay(1);
+            var data = await _assetDataService.GetSearchGridDataAsync(param);
+            Source.Clear();
+            foreach (var asset in data)
+            {
+                Source.Add(asset);
+            }
+        }
+        catch (Exception e)
+        {
+            await NotifyHelper.ShowNotifyDialog(NotifyHelper.ErrorTitle, e.Message);
+        }
+        finally
+        {
+            ProgressBarVisibility = Visibility.Collapsed;
+        }
+
+    }
+    public void AddToUpdateList(string key, SchoolAsset value)
+    {
+        _assetDataService.AddToUpdateList(key, value);
     }
 
     public async void OnNavigatedTo(object parameter)
     {
         Source.Clear();
         try
-        {      
+        {
             var data = await _assetDataService.GetGridDataAsync();
 
             foreach (var item in data)
@@ -106,13 +223,12 @@ public class AssetTableViewModel : ObservableRecipient, INavigationAware, INotif
             await NotifyHelper.ShowNotifyDialog(NotifyHelper.ErrorTitle, e.Message);
         }
     }
-
     public void OnNavigatedFrom()
     {
     }
-    
-    public event PropertyChangedEventHandler? PropertyChanged;
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+
+    public new event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual new void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
--- Views/AssetTablePage.xaml.cs
+++ Views/AssetTablePage.xaml.cs
@@ -2,6 +2,8 @@
 using AssetManager.Helpers;
 
 using Microsoft.UI.Xaml.Controls;
+using AssetManager.Core.Models;
+using Microsoft.UI.Xaml;
 
 namespace AssetManager.Views;
 
@@ -18,5 +20,21 @@ public sealed partial class AssetTablePage : Page
     {
         ViewModel = App.GetService<AssetTableViewModel>();
         InitializeComponent();
+    }
+
+    private async void SearchBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+    {
+        await ViewModel.SearchBoxQuery(args.QueryText);
+    }
+
+    private void DataGrid_RowEditEnded(object sender, CommunityToolkit.WinUI.UI.Controls.DataGridRowEditEndedEventArgs e)
+    {
+        SchoolAsset row = (SchoolAsset)e.Row.DataContext;
+        ViewModel.AddToUpdateList(row.AssetID.ToString(), row);
+    }
+
+    private void DataGrid_RightTapped(object sender, Microsoft.UI.Xaml.Input.RightTappedRoutedEventArgs e)
+    {
+        ViewModel.SelectedRow = (e.OriginalSource as FrameworkElement).DataContext as SchoolAsset;
     }
 }
