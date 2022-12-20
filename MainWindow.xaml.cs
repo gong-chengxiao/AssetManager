5956f980b80ca374f08813bc15e819d7357020d5
Chengxiao Gong <gcx@mail.nwpu.edu.cn>
2022-12-20 22:21:52 +0800

    改变初始长宽比例

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,5 +13,7 @@ public sealed partial class MainWindow : WindowEx
         AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
         Content = null;
         Title = "AppDisplayName".GetLocalized();
+        Width = 1280;
+        Height = 720;
     }
 }
